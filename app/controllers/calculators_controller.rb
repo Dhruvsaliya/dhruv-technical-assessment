59912ef9d73a8aa240d151aa5ff35b9d415be3b1
Dhruvsaliya <dhruvsaliya2769@gmail.com>
2024-07-12 12:55:55 +0530

    add multiple delimiter, consider negative numbers

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -2,7 +2,12 @@ class CalculatorsController < ApplicationController
   def create
     @numbers = params[:numbers]
     Rails.logger.debug "Submitted numbers: #{@numbers}"
-    @result = StringCalculator.add(@numbers)
+    begin
+      @result = StringCalculator.add(@numbers)
+    rescue StringCalculator::NegativeNumberError => e
+      @error = e.message
+    end
     Rails.logger.debug "Calculated result: #{@result}"
+    render :create, locals: { result: @result, error: @error }
   end
 end
--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,13 +1,34 @@
 class StringCalculator
+  class NegativeNumberError < StandardError; end
+  
   def self.add(numbers)
     return 0 if numbers.nil? || numbers.empty?
 
     Rails.logger.debug "input nubers #{numbers}"
 
-    numbers = numbers.gsub("\n", ',')
-    
-    sum = numbers.split(/[\s,]+/).map(&:strip).map(&:to_f).sum
+    # Check for custom delimiter
+    delimiter = ','
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers.split("\n", 2)
+      delimiter = delimiter[2..-1] 
+      numbers = numbers.gsub("\n", delimiter) 
+    else
+      numbers = numbers.gsub("\n", ',') 
+    end
 
+    Rails.logger.debug "Modified number after delimiter check: #{numbers}"
+
+    split_pattern = Regexp.escape(delimiter)
+    values = numbers.split(/[\s#{split_pattern}]+/).map(&:strip).map(&:to_f)
+    Rails.logger.debug "values: #{values}"
+
+    # Check for negative numbers
+    negatives = values.select { |num| num < 0 }
+    unless negatives.empty?
+      raise NegativeNumberError, "Negative numbers not allowed: #{negatives.join(', ')}"
+    end
+
+    sum = values.sum
     sum == sum.to_i ? sum.to_i : sum
   end
 end
